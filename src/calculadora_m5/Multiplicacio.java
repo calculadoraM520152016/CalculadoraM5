8eb561e78fe462d106f67e5f816c31ef2fc27b7c
smusaran <marcgomez@iesmontsia.org>
2016-02-15 16:14:35 +0100

    Multiplicacio Refactor

--- src/calculadora_m5/Multiplicacio.java
+++ src/calculadora_m5/Multiplicacio.java
@@ -7,31 +7,31 @@ package calculadora_m5;
  */
 public class Multiplicacio {
     public int opera(String [] array){
-        boolean positiu1 = true;
-        boolean positiu2 = true;
-        int res = 0;
-        int num1 = Integer.parseInt(array[0]);
-        int num2 = Integer.parseInt(array[1]);
+        boolean signePositiu1 = true;
+        boolean signePositiu2 = true;
+        int resultat = 0;
+        int numero1 = Integer.parseInt(array[0]);
+        int numero2 = Integer.parseInt(array[1]);
         
-        if(num1<0){
-            num1 = num1*-1;
-            positiu1=false;
+        if(numero1<0){
+            numero1 = numero1*-1;
+            signePositiu1=false;
         }
-        if(num2<0){
-            num2 = num2*-1;
-            positiu2=false;
-        }
-        
-        for (int i = 0; i<num1;i++){                //Sumem de forma iterativa el segon numero les vegaden que ho indiqui el primer.  
-            res = res +num2;
+        if(numero2<0){
+            numero2 = numero2*-1;
+            signePositiu2=false;
         }
         
-        if(!positiu1 && positiu2){
-            res = res*-1;
-        }else if(positiu1 && !positiu2){
-            res = res*-1;
+        for (int i = 0; i<numero1;i++){                //Sumem de forma iterativa el segon numero les vegaden que ho indiqui el primer.  
+            resultat = resultat +numero2;
         }
         
-        return res;
+        if(!signePositiu1 && signePositiu2){
+            resultat = resultat*-1;
+        }else if(signePositiu1 && !signePositiu2){
+            resultat = resultat*-1;
+        }
+        
+        return resultat;
     }
 }
